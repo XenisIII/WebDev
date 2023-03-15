cc1af12d311fef11d7f7ed4c04e6285eb460e368
Lilian-Monard <monardlilian1@gmail.com>
2023-03-15 11:17:45 +0100

    navbar.tpl

---
+++ templates_c/c2a008dcbf2b3220d94ef07bbc641d924abf9866_0.file.head.tpl.php
@@ -0,0 +1,41 @@
+<?php
+/* Smarty version 4.3.0, created on 2023-03-15 11:08:27
+  from 'C:\Users\daval\Documents\CESI\A2\Web\mvc\templates\head.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.0',
+  'unifunc' => 'content_6411991b543e64_86010197',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'c2a008dcbf2b3220d94ef07bbc641d924abf9866' => 
+    array (
+      0 => 'C:\\Users\\daval\\Documents\\CESI\\A2\\Web\\mvc\\templates\\head.tpl',
+      1 => 1678874268,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6411991b543e64_86010197 (Smarty_Internal_Template $_smarty_tpl) {
+?><!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $_smarty_tpl->tpl_vars['DocumentTitle']->value;?>
+</title>
+    <?php echo '<script'; ?>
+ src="https://cdn.tailwindcss.com/3.2.6"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ src="https://kit.fontawesome.com/8ed527737a.js" crossorigin="anonymous"><?php echo '</script'; ?>
+>
+</head>
+<?php }
+}
---
+++ templates_c/f1905a8c1abc3e05cc16d6f0dd2b9494b414bc22_0.file.test.tpl.php
@@ -0,0 +1,38 @@
+<?php
+/* Smarty version 4.3.0, created on 2023-03-15 11:10:38
+  from 'C:\Users\daval\Documents\CESI\A2\Web\mvc\templates\test.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.0',
+  'unifunc' => 'content_6411999ee0f1b2_81130123',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'f1905a8c1abc3e05cc16d6f0dd2b9494b414bc22' => 
+    array (
+      0 => 'C:\\Users\\daval\\Documents\\CESI\\A2\\Web\\mvc\\templates\\test.tpl',
+      1 => 1678875035,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_6411999ee0f1b2_81130123 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_loadInheritance();
+$_smarty_tpl->inheritance->init($_smarty_tpl, true);
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+
+<body>
+    
+</body>
+
+</html><?php $_smarty_tpl->inheritance->endChild($_smarty_tpl, "head.tpl");
+$_smarty_tpl->inheritance->endChild($_smarty_tpl, "navbar.tpl");
+}
+}
