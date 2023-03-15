72926b170c18a8a45faa7e0526e8622483574bd5
Lilian-Monard <monardlilian1@gmail.com>
2023-03-15 10:42:21 +0100

    creating repo

---
+++ src/lib/smarty.php
@@ -0,0 +1,14 @@
+<?php
+define('APP_DIR', __DIR__."\\..\\..\\");
+require("smarty/libs/Smarty.class.php");
+class AppSmarty extends Smarty{
+    function construct(){
+    parent::construct();
+    $this->setTemplateDir(APP_DIR . '\\templates');
+    $this->setCompileDir(APP_DIR . '\\templates_c');
+    $this->setConfigDir(APP_DIR . '\\configs');
+    $this->setCacheDir(APP_DIR . '\\cache');
+    }
+}
+echo APP_DIR;
+?>
