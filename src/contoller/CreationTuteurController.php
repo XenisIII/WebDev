9d342a368063d98cfea9753b746fc886de7ca1f1
BuildTools <unconfigured@null.spigotmc.org>
2023-03-22 21:15:50 +0100

    création tuteur + footer

---
+++ src/contoller/CreationTuteurController.php
@@ -0,0 +1,21 @@
+<?php
+require(__DIR__."\\..\\lib\\smarty.php");
+class CreationTuteurController{
+    private $smarty;
+    function __construct()
+    {
+        $this->smarty=new AppSmarty();
+    }
+    public function index(){
+        $this->smarty->assign("DocumentTitle","Création Tuteur");
+        $this->smarty->display('creationTuteur.tpl');
+    }
+    public function error(){
+        $this->smarty->assign("DocumentTitle","404 NOT FOUND");
+        $this->smarty->display('error.tpl');
+    }
+}
+/*$tmp_=new AppSmarty();
+$tmp_->assign("DocumentTitle","Démo");
+$tmp_->display('demo.tpl');*/
+?>
