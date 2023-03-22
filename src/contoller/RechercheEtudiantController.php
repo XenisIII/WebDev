5a1d75df34a382509fc39405a6bdf3275ead920c
XenisIII <xeos95@gmail.com>
2023-03-22 17:16:16 +0100

    TPL

---
+++ src/contoller/RechercheEtudiantController.php
@@ -0,0 +1,21 @@
+<?php
+require(__DIR__."\\..\\lib\\smarty.php");
+class RechercheEtudiantController{
+    private $smarty;
+    function __construct()
+    {
+        $this->smarty=new AppSmarty();
+    }
+    public function index(){
+        $this->smarty->assign("DocumentTitle","RechercheEtudiant");
+        $this->smarty->display('rechercheEtudiant.tpl');
+    }
+    public function error(){
+        $this->smarty->assign("DocumentTitle","404 NOT FOUND");
+        $this->smarty->display('error.tpl');
+    }
+}
+/*$tmp_=new AppSmarty();
+$tmp_->assign("DocumentTitle","Démo");
+$tmp_->display('demo.tpl');*/
+?>
