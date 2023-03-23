10fda3b0beef3375496a52e6b32590c3b3d5ad52
Lilian-Monar <monardlilian1@gmail.com>
2023-03-23 11:55:44 +0100

    fin page profil

--- templates/profil.tpl
+++ templates/profil.tpl
@@ -26,16 +26,135 @@
         </div>
     </div>
     <!-- wishList-->
-    <div class="my-5 mx-5 border border-black h-full">
+    <div class="my-5 mx-5 border border-black h-full overflow-y-scroll max-h-[100%]">
         <div class="border-b border-black text-center text-2xl h-12">
         <h1>
             FAVORIS
         </h1>
         </div>
         <div>
-            <ul>
-                <li>offre 1 </li>
-            </ul>
+        <div class="bg-white divide-y w-[90%] sm:w-[95%] h-[500px] mx-[5%] my-[2%] sm:mx-[0%] sm:my[0%] lg:ml-[2%]">
+        <div class="flex flex-row sm:flex-row sm:flex-wrap sm:justify-between 
+        w-[90%] sm:w-[95%] h-16 mx-[5%] mb-[2%] sm:mx-[0%] sm:my[0%] sm:mt-[2%] lg:ml-[2%]  ">
+            <div class="flex flex-col ">
+                <div class=''>
+                    <h2 class="mb-2 text-sm font-medium text-xl"> Ceci est un post </h2>
+                </div>
+                <div class="invisible sm:visible">
+                    <i class="fa fa-building"></i> Orange
+                    <i class="fa fa-location-dot"></i> Orléans
+                    <i class="fa fa-clock"></i> 3 mois
+                </div>
+            </div>
+            <div class="flex items-stretch self-center">
+                <i class="fa fa-star cursor-pointer text-yellow-300" name="star1" id="star1"
+                    onclick="star(1)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star2" id="star2" onclick="star(2)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star3" id="star3" onclick="star(3)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star4" id="star4" onclick="star(4)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star5" id="star5" onclick="star(5)"></i>
+            </div>
+            <div class="flex items-stretch self-center">
+                <i id="heart" class="fa fa-heart m-8 cursor-pointer" onclick="heartv2()"></i>
+            </div>
+        </div>
+        <div class="flex flex-row sm:flex-row sm:flex-wrap sm:justify-between 
+        w-[90%] sm:w-[95%] h-16 mx-[5%] mb-[2%] sm:mx-[0%] sm:my[0%] sm:mt-[2%] lg:ml-[2%]">
+            <div class="flex flex-col ">
+                <div>
+                    <h2 class="mb-2 text-sm font-medium text-xl"> Ceci est un post </h2>
+                </div>
+                <div class="invisible sm:visible">
+                    <i class="fa fa-building"></i> Orange
+                    <i class="fa fa-location-dot"></i> Orléans
+                    <i class="fa fa-clock"></i> 3 mois
+                </div>
+            </div>
+            <div class="flex items-stretch self-center">
+                <i class="fa fa-star cursor-pointer text-yellow-300" name="star1" id="star1"
+                    onclick="star(1)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star2" id="star2" onclick="star(2)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star3" id="star3" onclick="star(3)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star4" id="star4" onclick="star(4)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star5" id="star5" onclick="star(5)"></i>
+            </div>
+            <div class="flex items-stretch self-center">
+                <i class="fa fa-heart m-8"></i>
+            </div>
+        </div>
+        <div class="flex flex-row sm:flex-row sm:flex-wrap sm:justify-between 
+        w-[90%] sm:w-[95%] h-16 mx-[5%] mb-[2%] sm:mx-[0%] sm:my[0%] sm:mt-[2%] lg:ml-[2%]">
+            <div class="flex flex-col ">
+                <div>
+                    <h2 class="mb-2 text-sm font-medium text-xl"> Ceci est un post </h2>
+                </div>
+                <div class="mb-0 invisible sm:visible">
+                    <i class="fa fa-building"></i> Orange
+                    <i class="fa fa-location-dot"></i> Orléans
+                    <i class="fa fa-clock"></i> 3 mois
+                </div>
+            </div>
+            <div class="flex items-stretch self-center">
+                <i class="fa fa-star cursor-pointer text-yellow-300" name="star1" id="star1"
+                    onclick="star(1)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star2" id="star2" onclick="star(2)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star3" id="star3" onclick="star(3)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star4" id="star4" onclick="star(4)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star5" id="star5" onclick="star(5)"></i>
+            </div>
+            <div class="flex items-stretch self-center">
+                <i class="fa fa-heart m-8"></i>
+            </div>
+        </div>
+        <div class="flex flex-row sm:flex-row sm:flex-wrap sm:justify-between 
+        w-[90%] sm:w-[95%] h-16 mx-[5%] mb-[2%] sm:mx-[0%] sm:my[0%] sm:mt-[2%] lg:ml-[2%] ">
+            <div class="flex flex-col ">
+                <div>
+                    <h2 class="mb-2 text-sm font-medium text-xl"> Ceci est un post </h2>
+                </div>
+                <div class="mb-0 invisible sm:visible">
+                    <i class="fa fa-building"></i> Orange
+                    <i class="fa fa-location-dot"></i> Orléans
+                    <i class="fa fa-clock"></i> 3 mois
+                </div>
+            </div>
+            <div class="flex items-stretch self-center">
+                <i class="fa fa-star cursor-pointer text-yellow-300" name="star1" id="star1"
+                    onclick="star(1)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star2" id="star2" onclick="star(2)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star3" id="star3" onclick="star(3)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star4" id="star4" onclick="star(4)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star5" id="star5" onclick="star(5)"></i>
+            </div>
+            <div class="flex items-stretch self-center">
+                <i class="fa fa-heart m-8"></i>
+            </div>
+        </div>
+        <div class="flex flex-row sm:flex-row sm:flex-wrap sm:justify-between 
+        w-[90%] sm:w-[95%] h-16 mx-[5%] mb-[2%] sm:mx-[0%] sm:my[0%] sm:mt-[2%] lg:ml-[2%]">
+            <div class="flex flex-col ">
+                <div>
+                    <h2 class="mb-2 text-sm font-medium text-xl"> Ceci est un post </h2>
+                </div>
+                <div class="invisible sm:visible">
+                    <i class="fa fa-building"></i> Orange
+                    <i class="fa fa-location-dot"></i> Orléans
+                    <i class="fa fa-clock"></i> 3 mois
+                </div>
+            </div>
+            <div class="flex items-stretch self-center">
+                <i class="fa fa-star cursor-pointer text-yellow-300" name="star1" id="star1"
+                    onclick="star(1)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star2" id="star2" onclick="star(2)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star3" id="star3" onclick="star(3)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star4" id="star4" onclick="star(4)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star5" id="star5" onclick="star(5)"></i>
+            </div>
+            <div class="flex items-stretch self-center">
+                <i class="fa fa-heart m-8"></i>
+                <!---text-red-500-->
+            </div>
+        </div>
         </div>
     
 
--- templates_c/5d04ce0423edfff48e1fd1b15da2e7dcc49075cd_0.file.profil.tpl.php
+++ templates_c/5d04ce0423edfff48e1fd1b15da2e7dcc49075cd_0.file.profil.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.0, created on 2023-03-22 16:11:21
+/* Smarty version 4.3.0, created on 2023-03-23 11:55:12
   from '/home/lilian/Documents/webdev/WebDev-mvc/templates/profil.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.0',
-  'unifunc' => 'content_641b1a99787d51_19458384',
+  'unifunc' => 'content_641c30106beaa1_20353878',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '5d04ce0423edfff48e1fd1b15da2e7dcc49075cd' => 
     array (
       0 => '/home/lilian/Documents/webdev/WebDev-mvc/templates/profil.tpl',
-      1 => 1679497880,
+      1 => 1679568911,
       2 => 'file',
     ),
   ),
@@ -22,7 +22,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:navbar.tpl' => 1,
   ),
 ),false)) {
-function content_641b1a99787d51_19458384 (Smarty_Internal_Template $_smarty_tpl) {
+function content_641c30106beaa1_20353878 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender("file:head.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 <body class="bg-[#f5f5f5] h-screen">
@@ -53,16 +53,135 @@ $_smarty_tpl->_subTemplateRender("file:head.tpl", $_smarty_tpl->cache_id, $_smar
         </div>
     </div>
     <!-- wishList-->
-    <div class="my-5 mx-5 border border-black h-full">
+    <div class="my-5 mx-5 border border-black h-full overflow-y-scroll max-h-[100%]">
         <div class="border-b border-black text-center text-2xl h-12">
         <h1>
             FAVORIS
         </h1>
         </div>
         <div>
-            <ul>
-                <li>offre 1 </li>
-            </ul>
+        <div class="bg-white divide-y w-[90%] sm:w-[95%] h-[500px] mx-[5%] my-[2%] sm:mx-[0%] sm:my[0%] lg:ml-[2%]">
+        <div class="flex flex-row sm:flex-row sm:flex-wrap sm:justify-between 
+        w-[90%] sm:w-[95%] h-16 mx-[5%] mb-[2%] sm:mx-[0%] sm:my[0%] sm:mt-[2%] lg:ml-[2%]  ">
+            <div class="flex flex-col ">
+                <div class=''>
+                    <h2 class="mb-2 text-sm font-medium text-xl"> Ceci est un post </h2>
+                </div>
+                <div class="invisible sm:visible">
+                    <i class="fa fa-building"></i> Orange
+                    <i class="fa fa-location-dot"></i> Orléans
+                    <i class="fa fa-clock"></i> 3 mois
+                </div>
+            </div>
+            <div class="flex items-stretch self-center">
+                <i class="fa fa-star cursor-pointer text-yellow-300" name="star1" id="star1"
+                    onclick="star(1)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star2" id="star2" onclick="star(2)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star3" id="star3" onclick="star(3)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star4" id="star4" onclick="star(4)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star5" id="star5" onclick="star(5)"></i>
+            </div>
+            <div class="flex items-stretch self-center">
+                <i id="heart" class="fa fa-heart m-8 cursor-pointer" onclick="heartv2()"></i>
+            </div>
+        </div>
+        <div class="flex flex-row sm:flex-row sm:flex-wrap sm:justify-between 
+        w-[90%] sm:w-[95%] h-16 mx-[5%] mb-[2%] sm:mx-[0%] sm:my[0%] sm:mt-[2%] lg:ml-[2%]">
+            <div class="flex flex-col ">
+                <div>
+                    <h2 class="mb-2 text-sm font-medium text-xl"> Ceci est un post </h2>
+                </div>
+                <div class="invisible sm:visible">
+                    <i class="fa fa-building"></i> Orange
+                    <i class="fa fa-location-dot"></i> Orléans
+                    <i class="fa fa-clock"></i> 3 mois
+                </div>
+            </div>
+            <div class="flex items-stretch self-center">
+                <i class="fa fa-star cursor-pointer text-yellow-300" name="star1" id="star1"
+                    onclick="star(1)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star2" id="star2" onclick="star(2)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star3" id="star3" onclick="star(3)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star4" id="star4" onclick="star(4)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star5" id="star5" onclick="star(5)"></i>
+            </div>
+            <div class="flex items-stretch self-center">
+                <i class="fa fa-heart m-8"></i>
+            </div>
+        </div>
+        <div class="flex flex-row sm:flex-row sm:flex-wrap sm:justify-between 
+        w-[90%] sm:w-[95%] h-16 mx-[5%] mb-[2%] sm:mx-[0%] sm:my[0%] sm:mt-[2%] lg:ml-[2%]">
+            <div class="flex flex-col ">
+                <div>
+                    <h2 class="mb-2 text-sm font-medium text-xl"> Ceci est un post </h2>
+                </div>
+                <div class="mb-0 invisible sm:visible">
+                    <i class="fa fa-building"></i> Orange
+                    <i class="fa fa-location-dot"></i> Orléans
+                    <i class="fa fa-clock"></i> 3 mois
+                </div>
+            </div>
+            <div class="flex items-stretch self-center">
+                <i class="fa fa-star cursor-pointer text-yellow-300" name="star1" id="star1"
+                    onclick="star(1)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star2" id="star2" onclick="star(2)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star3" id="star3" onclick="star(3)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star4" id="star4" onclick="star(4)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star5" id="star5" onclick="star(5)"></i>
+            </div>
+            <div class="flex items-stretch self-center">
+                <i class="fa fa-heart m-8"></i>
+            </div>
+        </div>
+        <div class="flex flex-row sm:flex-row sm:flex-wrap sm:justify-between 
+        w-[90%] sm:w-[95%] h-16 mx-[5%] mb-[2%] sm:mx-[0%] sm:my[0%] sm:mt-[2%] lg:ml-[2%] ">
+            <div class="flex flex-col ">
+                <div>
+                    <h2 class="mb-2 text-sm font-medium text-xl"> Ceci est un post </h2>
+                </div>
+                <div class="mb-0 invisible sm:visible">
+                    <i class="fa fa-building"></i> Orange
+                    <i class="fa fa-location-dot"></i> Orléans
+                    <i class="fa fa-clock"></i> 3 mois
+                </div>
+            </div>
+            <div class="flex items-stretch self-center">
+                <i class="fa fa-star cursor-pointer text-yellow-300" name="star1" id="star1"
+                    onclick="star(1)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star2" id="star2" onclick="star(2)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star3" id="star3" onclick="star(3)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star4" id="star4" onclick="star(4)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star5" id="star5" onclick="star(5)"></i>
+            </div>
+            <div class="flex items-stretch self-center">
+                <i class="fa fa-heart m-8"></i>
+            </div>
+        </div>
+        <div class="flex flex-row sm:flex-row sm:flex-wrap sm:justify-between 
+        w-[90%] sm:w-[95%] h-16 mx-[5%] mb-[2%] sm:mx-[0%] sm:my[0%] sm:mt-[2%] lg:ml-[2%]">
+            <div class="flex flex-col ">
+                <div>
+                    <h2 class="mb-2 text-sm font-medium text-xl"> Ceci est un post </h2>
+                </div>
+                <div class="invisible sm:visible">
+                    <i class="fa fa-building"></i> Orange
+                    <i class="fa fa-location-dot"></i> Orléans
+                    <i class="fa fa-clock"></i> 3 mois
+                </div>
+            </div>
+            <div class="flex items-stretch self-center">
+                <i class="fa fa-star cursor-pointer text-yellow-300" name="star1" id="star1"
+                    onclick="star(1)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star2" id="star2" onclick="star(2)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star3" id="star3" onclick="star(3)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star4" id="star4" onclick="star(4)"></i>
+                <i class="fa fa-star cursor-pointer text-black" name="star5" id="star5" onclick="star(5)"></i>
+            </div>
+            <div class="flex items-stretch self-center">
+                <i class="fa fa-heart m-8"></i>
+                <!---text-red-500-->
+            </div>
+        </div>
         </div>
     
 
