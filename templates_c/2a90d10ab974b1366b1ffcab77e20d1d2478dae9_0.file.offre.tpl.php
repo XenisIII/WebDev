2ac0df8827ea36c2710e37eb6725c1e6c3e5c2e5
Lilian-Monar <monardlilian1@gmail.com>
2023-03-24 10:10:59 +0100

    debug offre

--- templates_c/2a90d10ab974b1366b1ffcab77e20d1d2478dae9_0.file.offre.tpl.php
+++ templates_c/2a90d10ab974b1366b1ffcab77e20d1d2478dae9_0.file.offre.tpl.php
@@ -1,7 +1,35 @@
-{include file="head.tpl"}
+<?php
+/* Smarty version 4.3.0, created on 2023-03-24 10:05:52
+  from '/home/lilian/Documents/git/WebDev-mvc/templates/offre.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '4.3.0',
+  'unifunc' => 'content_641d67f0532956_79602444',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '2a90d10ab974b1366b1ffcab77e20d1d2478dae9' => 
+    array (
+      0 => '/home/lilian/Documents/git/WebDev-mvc/templates/offre.tpl',
+      1 => 1679648747,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+    'file:head.tpl' => 1,
+    'file:navbar.tpl' => 1,
+    'file:footer.tpl' => 1,
+  ),
+),false)) {
+function content_641d67f0532956_79602444 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->_subTemplateRender("file:head.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
 <body class="bg-[#f5f5f5]">
 <div>
-{include file="navbar.tpl"}
+<?php $_smarty_tpl->_subTemplateRender("file:navbar.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
 </div>
 <!-- Box globale-->
 <div class="bg-[#f5f5f5] flex justify-center min-[880px]:h-screen h-[100%]">
@@ -12,27 +40,39 @@
         <div>
             <!-- Titre -->
             <div class="mt-3 text-2xl">
-                <h3>{$nomOffre}</h3>
+                <h3><?php echo $_smarty_tpl->tpl_vars['nomOffre']->value;?>
+</h3>
             </div>
             <!-- Compétences-->
             <div class="max-[880px]:w-9/12 w-full">
                 <ul class="flex flex-wrap my-5">
-                    {foreach $Competences as $competence}
-                        <li class="rounded-full bg-gray-200 px-5 mx-2 mt-2">{$competence->competence}</li>
-                    {/foreach}
+                    <?php
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['Competences']->value, 'competence');
+$_smarty_tpl->tpl_vars['competence']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['competence']->value) {
+$_smarty_tpl->tpl_vars['competence']->do_else = false;
+?>
+                        <li class="rounded-full bg-gray-200 px-5 mx-2 mt-2"><?php echo $_smarty_tpl->tpl_vars['competence']->value->competence;?>
+</li>
+                    <?php
+}
+$_smarty_tpl->smarty->ext->_foreach->restore($_smarty_tpl, 1);?>
                 </ul>
             </div>
             <!--durée-->
             <div class="my-2">
             <p class="text-xl">
                 <i class="fa-solid fa-stopwatch"></i>
-                 {$Duree} Mois
+                 <?php echo $_smarty_tpl->tpl_vars['Duree']->value;?>
+ Mois
             </p>
             <p>
-                Date de début : {$DateDebut}
+                Date de début : <?php echo $_smarty_tpl->tpl_vars['DateDebut']->value;?>
+
             </p>
             <p>
-                Date de mise en ligne  : {$DatePoste}
+                Date de mise en ligne  : <?php echo $_smarty_tpl->tpl_vars['DatePoste']->value;?>
+
             </p>
             </div>
         </div>
@@ -41,7 +81,8 @@
             <div class="mx-2">
                 <p class="text-3xl">
                 <i class="fa-regular fa-building"></i>
-                    {$NomEntreprise}
+                    <?php echo $_smarty_tpl->tpl_vars['NomEntreprise']->value;?>
+
                 </p>
             </div>
             <div class="ml-3">
@@ -54,7 +95,8 @@
             </div>
             <div class="ml-2">
                 <i class="fa fa-compassEntreprise fa-xl"></i>
-                {$Localite}
+                <?php echo $_smarty_tpl->tpl_vars['Localite']->value;?>
+
             </div>
         </div>
         <!-- Bouton-->
@@ -71,7 +113,8 @@
         </div>
         <!-- information-->
         <div class="border border-black rounded min-[880px]:h-[70%] overflow-y-scroll max-h-96">
-            <p class="h-full p-2 min-[880px]:p-7  text-md ">{$info}</p>
+            <p class="h-full p-2 min-[880px]:p-7  text-md "><?php echo $_smarty_tpl->tpl_vars['info']->value;?>
+</p>
             </div>
         <div class="flex justify-between min-[880px]:hidden h-3/5 items-center min-h-12 mt-3">
             <button class="border border-black w-3/12 h-3/5 min-w-fit min-h-fit rounded max-[880px]:h-ful hover:bg-slate-50 " onclick="window.location='/index.php/rechercheStage'">Retour</button>
@@ -81,7 +124,8 @@
     </div>
 </div>
 </div>
-<script>
+<?php echo '<script'; ?>
+>
 function star(etoile){
     switch(etoile){
         case 1:
@@ -111,8 +155,12 @@ function star(etoile){
             break;
     }
 }
-star({$NoteTuteur})
-</script>
-{include file="footer.tpl"}
+star(<?php echo $_smarty_tpl->tpl_vars['NoteTuteur']->value;?>
+)
+<?php echo '</script'; ?>
+>
+<?php $_smarty_tpl->_subTemplateRender("file:footer.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
+?>
 </body>
-</html>
+</html><?php }
+}
