bcc79851e3046f18c2bd8c7760551da61ac9d8bf
heavenlm <louis.marteau0307@gmail.com>
2023-03-28 13:36:43 +0200

    creationEntreprise API ville et CP + ajout JQuery

--- templates/head.tpl
+++ templates/head.tpl
@@ -10,5 +10,6 @@
     <script src="https://cdn.tailwindcss.com/3.2.6"></script>
     <script src="https://kit.fontawesome.com/8ed527737a.js" crossorigin="anonymous"></script>
     <link rel="manifest" href="/../script/manifest.json">
-    <script src="/service_worker.js"></script>
+    <script type="text/javascript" src="/service_worker.js"></script>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
 </head>
--- templates_c/2596bebf2646c0c1aecaeb070d135f391e9b9b35_0.file.head.tpl.php
+++ templates_c/2596bebf2646c0c1aecaeb070d135f391e9b9b35_0.file.head.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 4.3.0, created on 2023-03-28 09:30:48
+/* Smarty version 4.3.0, created on 2023-03-28 11:25:35
   from 'C:\Users\pc\Desktop\Prosit\A2\WEB\livrable\WebDev\WebDev-mvc\templates\head.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '4.3.0',
-  'unifunc' => 'content_642297a860bf64_55781892',
+  'unifunc' => 'content_6422b28f74fda4_39838350',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '2596bebf2646c0c1aecaeb070d135f391e9b9b35' => 
     array (
       0 => 'C:\\Users\\pc\\Desktop\\Prosit\\A2\\WEB\\livrable\\WebDev\\WebDev-mvc\\templates\\head.tpl',
-      1 => 1679919759,
+      1 => 1679995533,
       2 => 'file',
     ),
   ),
@@ -20,7 +20,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   array (
   ),
 ),false)) {
-function content_642297a860bf64_55781892 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6422b28f74fda4_39838350 (Smarty_Internal_Template $_smarty_tpl) {
 ?><!DOCTYPE html>
 <html lang="fr">
 
@@ -39,7 +39,10 @@ function content_642297a860bf64_55781892 (Smarty_Internal_Template $_smarty_tpl)
 >
     <link rel="manifest" href="/../script/manifest.json">
     <?php echo '<script'; ?>
- src="/service_worker.js"><?php echo '</script'; ?>
+ type="text/javascript" src="/service_worker.js"><?php echo '</script'; ?>
+>
+    <?php echo '<script'; ?>
+ type="text/javascript" src="https://code.jquery.com/jquery-1.12.4.min.js"><?php echo '</script'; ?>
 >
 </head>
 <?php }
